6e842d4cd4df6ec9a7bc483bc4df9f5afe377aa2
rtavares87 <rtavaresrj87@gmail.com>
2018-10-08 00:07:54 -0300

    ...

--- AppraisalSpark/src/main/scala/appraisal/spark/entities/Entities.scala
+++ AppraisalSpark/src/main/scala/appraisal/spark/entities/Entities.scala
@@ -8,7 +8,7 @@ object Entities {
   final case class Result(lineId: Long, originalValue: Option[Double], imputationValue: Double,
       error: Option[Double] = null, percentualError: Option[Double] = null)
   
-  final case class ImputationResult(result: RDD[Result], avgError: Option[Double] = null,
+  final case class ImputationResult(result: RDD[Result]= null, avgError: Option[Double] = null,
       totalError: Option[Double] = null, avgPercentError: Option[Double] = null)  extends StrategyResult
   
   final case class CResult(cluster: Int, lineId: Long)
--- AppraisalSpark/src/main/scala/appraisal/spark/entities/ImputationPlan.scala
+++ AppraisalSpark/src/main/scala/appraisal/spark/entities/ImputationPlan.scala
@@ -3,6 +3,9 @@ package appraisal.spark.entities
 import appraisal.spark.interfaces._
 import org.apache.spark.sql._
 import appraisal.spark.strategies._
+import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.sql.types.{DoubleType, StringType, StructField, StructType}
+import org.apache.spark.sql.types.{LongType, IntegerType}
 
 class ImputationPlan {
   
@@ -31,17 +34,40 @@ class ImputationPlan {
         
       }
       
+      var imputationBatch :org.apache.spark.rdd.RDD[DataFrame] = null
+      
       if (strategy.isInstanceOf[ClusteringStrategy]) {
         
         val cs = strategy.asInstanceOf[ClusteringStrategy]
         cs.params.update("features", features)
         val cr = cs.run(edf)
         
+        val schema = new StructType()
+        .add(StructField("cluster", IntegerType, true))
+        .add(StructField("lineidcluster", LongType, true))
+        
+        val lineIdPos = edf.columns.indexOf("lineId")
+        val context = edf.sparkSession.sparkContext
+        val bedf = context.broadcast(edf)
+        
+        var cdf = edf.sparkSession.createDataFrame(cr.result.map(x => Row(x.cluster, x.lineId)), schema)
+        var ccdf = cdf.rdd.map(r => (r.getInt(0), bedf.value.filter(l => l.getLong(lineIdPos) == r.getLong(1)).first()))
+        
+        imputationBatch = cdf.rdd.map(_.getInt(0)).distinct().map(x => ccdf.filter(r => r._1 == x)
+            .map(_._2)).map(z => cdf.sparkSession.createDataFrame(z, bedf.value.schema))
+        
       }
       
+      if(imputationBatch == null) imputationBatch = edf.sparkSession.sparkContext.parallelize(Seq(edf))
+      
       if (strategy.isInstanceOf[ImputationStrategy]) {
-        
-        
+          
+         val is = strategy.asInstanceOf[ImputationStrategy]
+         is.params.update("features", features)
+         
+         filtrar dfs que possuam nulo na coluna imputada 
+         
+         val irs = imputationBatch.map(x => is.run(x))
         
       }
       
